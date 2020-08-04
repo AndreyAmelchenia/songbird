f19b64ecf684dd654a595e185c8cc76496a8bfcb
Andey Amelchenia <pmad1986@gmail.com>
2020-08-04 13:01:42 +0300

    feat: add header, main, footer

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import Header from '../header/Header';
+import Main from '../main/Main';
+import Footer from '../footer/Footer';
 
 const App = () => (
-  <div>Songbird</div>
+  <>
+    <Header />
+    <Main />
+    <Footer />
+  </>
 );
 export default App;
---
+++ src/main/Main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Main = () => (
+  <main>main</main>
+);
+export default Main;
