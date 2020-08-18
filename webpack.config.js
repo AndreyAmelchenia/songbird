f9c25a771750a9c2251219312df92bbd11b39282
Andey Amelchenia <pmad1986@gmail.com>
2020-08-18 16:24:28 +0300

    fix: copy _redirects

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,9 @@ module.exports = {
           from: path.resolve(__dirname, 'src/assets/'),
           to: path.resolve(__dirname, 'dist/assets/'),
         },
+        {
+          from: path.resolve(__dirname, 'src/', '_redirects'),
+        },
       ],
     }),
     new MiniCss({
